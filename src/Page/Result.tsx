efe96e1a8f0babb435470243cecb1341def12762
Adrian Finantyo <adrianfinantyo@gmail.com>
2022-11-21 21:02:20 +0700

    fix: err

--- src/Page/Result.tsx
+++ src/Page/Result.tsx
@@ -1,9 +1,17 @@
-import { Box, Center, Text } from "@chakra-ui/react";
+import { Box, Center, Text, VStack } from "@chakra-ui/react";
 import { fade, transition } from "../Shared/Animation";
 import { FlexMotion } from "../Shared/ChakraMotion";
 
 const Result: React.FC = () => {
-  return <Center>Progre</Center>;
+  return (
+    <Center>
+      <VStack>
+        <Text fontSize="2xl">Hasil Tes</Text>
+        <Text fontSize="xl">Anda memiliki gejala depresi</Text>
+        <Text fontSize="xl">Silahkan hubungi psikolog</Text>
+      </VStack>
+    </Center>
+  );
 };
 
 export default Result;
