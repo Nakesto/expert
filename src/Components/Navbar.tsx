027a81cea5994012638b80de38835bad35dbf77f
Vincent G <93830263+Nakesto@users.noreply.github.com>
2022-12-01 22:33:41 +0700

    Update Navbar.tsx

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <Box width="100%">
+    <Box width="100%" bgColor ="teal.400">
       <HStack
         maxWidth="80%"
         justifyContent="space-between"
@@ -13,17 +13,23 @@ const Navbar = () => {
         paddingTop="20px"
         paddingBottom="20px"
       >
-        <Text fontSize="3xl" fontWeight="bold">
+        <Text fontSize="3xl" fontWeight="bold" textColor="white">
           Depression
         </Text>
         <Box width="20%" />
         <Flex direction="row" flex="1" justifyContent="end" alignItems="center">
-          <Link to="/">Home</Link>
-          <Link to="/about" style={{ marginLeft: "20px" }}>
-            <Text backgroundColor="transparent">About Us</Text>
+          <Link to="/">
+            <Text 
+              backgroundColor="transparent" 
+              textColor="white" 
+              fontWeight="bold">
+                Home
+            </Text>
           </Link>
           <Link to="/test" style={{ marginLeft: "40px" }}>
-            <Button>Depression Test</Button>
+            <Button>
+              <Text textColor="teal.400"> Depression Test </Text>
+            </Button>
           </Link>
         </Flex>
       </HStack>
